65e27b5a82efc592cb7d2221e24575628516f5b5
dtlam2601 <dtlam2601@gmail.com>
2023-10-23 14:49:23 +0700

    Update kafka.sh

--- week_6_stream_processing/kafka.sh
+++ week_6_stream_processing/kafka.sh
@@ -1,2 +1,10 @@
 # list all topic
-bin/kafka-topics.sh --list --zookeeper localhost:2181
+bin/kafka-topics.sh --list --bootstrap-server localhost:9092
+
+
+#Kafka CONNECT-----------------------------
+# 2 stand-alone connectors (run in a single,local,dedicated process)
+bin/connect-standalone.sh \ 
+  config/connect-standalone.properties \
+  config/connect-file-source.properties \
+  config/connect-file-sink.properties
